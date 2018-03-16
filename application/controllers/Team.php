9687941a4985cb59c7401375abf360737538aa86
agungdh <agungdh@live.com>
2018-03-16 17:32:41 +0700

    update

---
+++ application/controllers/Team.php
@@ -0,0 +1,86 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Team extends CI_Controller {
+	var $tabel;
+
+	function __construct(){
+		parent::__construct();
+		$this->tabel = "mteammhs";
+	}
+
+	function index($nokegiatan) {
+		$data['isi'] = "team/index";
+		$data['data']['kegiatan'] = $this->db->get_where('mkegiatan', array('cnokegiatan' => $nokegiatan))->row();
+		$data['data']['team'] = $this->db->get($this->tabel)->result();
+		$this->load->view("template/template", $data);
+	}
+
+	function tambah($nokegiatan) {
+		$data['isi'] = "team/tambah";
+		$data['data']['kegiatan'] = $this->db->get_where('mkegiatan', array('cnokegiatan' => $nokegiatan))->row();
+
+		$this->load->view("template/template", $data);
+	}
+
+	function aksi_tambah($nokegiatan) {		
+		$data['cnoteam'] = $this->input->post('noteam');
+		$data['cnmteam'] = $this->input->post('team');
+		$data['cjmlagt'] = $this->input->post('jumlah');
+		$data['csmt'] = $this->input->post('semester');
+		$data['cthnajar'] = $this->input->post('tahunajar1') . $this->input->post('tahunajar2');
+		$data['ctglawallomba'] = $this->input->post('tanggalawal');
+		$data['ctglakhirlomba'] = $this->input->post('tanggalakhir');
+		$data['ctempatlomba'] = $this->input->post('tempatlomba');
+		$data['cprestasi'] = $this->input->post('prestasi');
+		$data['cbukti'] = $this->input->post('bukti');
+		$data['cfoto'] = $this->input->post('foto');
+		$data['cuserentri'] = $this->session->cuserentri;
+		$data['ctglentri'] = date('Y-m-d');
+		$data['cnokegiatan'] = $nokegiatan;
+		$this->db->insert(
+			$this->tabel,
+			$data
+		);
+
+		redirect(base_url('team/index/' . $nokegiatan));
+	}
+
+	function ubah($nokegiatan, $noteam) {
+		$data['isi'] = "team/ubah";
+		$data['data']['kegiatan'] = $this->db->get_where('mkegiatan', array('cnokegiatan' => $nokegiatan))->row();
+		$where['cnoteam'] = $noteam;
+		$data['data']['team'] = $this->db->get_where(
+			$this->tabel,
+			$where
+		)->row();
+
+		$this->load->view("template/template", $data);
+	}
+
+	function aksi_ubah($nokegiatan) {
+		$data['data']['kegiatan'] = $this->db->get_where('mkegiatan', array('cnokegiatan' => $nokegiatan))->row();
+		$data['cnoteam'] = $this->input->post('noteam');
+		$data['cnmteam'] = $this->input->post('team');
+		$where['cnoteam'] = $data['cnoteam'];
+		
+		$this->db->update(
+			$this->tabel,
+			$data,
+			$where
+		);
+
+		redirect(base_url('team/index/' . $nokegiatan));
+	}
+
+	function aksi_hapus($nokegiatan, $noteam) {
+		$where['cnoteam'] = $noteam;
+		$this->db->delete(
+			$this->tabel,
+			$where
+		);
+
+		redirect(base_url('team/index/' . $nokegiatan));
+	}
+
+}
---
+++ application/views/team/index.php
@@ -0,0 +1,79 @@
+<?php 
+// var_dump($data['versi_borang']);
+// exit();
+?>
+<script type="text/javascript" language="javascript" >
+  var dTable;
+  $(document).ready(function() {
+    dTable = $('#lookup').DataTable({
+      responsive: true
+    });
+  });
+</script>
+
+<div class="box box-primary">
+  <div class="box-header with-border">
+    <h4><strong><font color=blue>TEAM</font></strong></h4>
+  </div><!-- /.box-header -->
+
+    <div class="box-body">
+
+    <div class="form-group">
+      <a href='<?php echo base_url("team/tambah/" . $data['kegiatan']->cnokegiatan); ?>'><button class="btn btn-success"><i class="fa fa-plus"></i> Team</button></a>
+    </div>
+
+    <table id="lookup" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+      <thead>
+        <tr>
+                    <th>NO TEAM</th>
+                    <th>TEAM</th>
+                    <th>JUMLAH ANGGOTA</th>
+                    <th>SEMESTER</th>
+                    <th>TAHUN AJAR</th>
+                    <th>TANGGAL AWAL LOMBA</th>
+                    <th>TANGGAL AKHIR LOMBA</th>
+                    <th>TEMPAT LOMBA</th>
+                    <th>PRESTASI</th>
+                    <th>BUKTI</th>
+                    <th>PROSES</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <?php
+        foreach ($data['team'] as $item) {
+          ?>
+          <tr>
+            <th><?php echo $item->cnoteam; ?></th>
+            <th><?php echo $item->cnmteam; ?></th>
+            <th><?php echo $item->cjmlagt; ?></th>
+            <th><?php echo $item->csmt == 'o' ? 'Gasal' : 'Genap'; ?></th>
+            <th><?php echo $item->cthnajar; ?></th>
+            <th><?php echo $this->pustaka->tanggal_indo($item->ctglawallomba); ?></th>
+            <th><?php echo $this->pustaka->tanggal_indo($item->ctglakhirlomba); ?></th>
+            <th><?php echo $item->ctempatlomba; ?></th>
+            <th><?php echo $item->cprestasi; ?></th>
+            <th><?php echo $item->cbukti; ?></th>
+            <th><?php echo $item->cfoto; ?></th>
+              <th>
+                <a class="btn btn-primary" href="<?php echo base_url('anggota/index/'.$item->cnoteam) ?>"> <i class="fa fa-share"></i> Anggota</a>
+                <a class="btn btn-info" href="<?php echo base_url('team/ubah/'.$item->cnoteam) ?>"> <i class="fa fa-pencil"></i></a>
+                <a class="btn btn-danger" onclick="hapus('<?php echo $item->cnoteam; ?>')"> <i class="fa fa-trash"></i></a>
+              </th>
+          </tr>
+          <?php
+        }
+        ?>
+      </tbody>
+      
+    </table>
+  </div><!-- /.boxbody -->
+</div><!-- /.box -->
+
+<script type="text/javascript">
+function hapus(id) {
+  if (confirm("Yakin hapus ?")) {
+    window.location = "<?php echo base_url('team/aksi_hapus/' . $data['kegiatan']->cnokegiatan . '/'); ?>" + id;
+  }
+}
+</script>
