3e4e09ad13ce1a1fdea586ff786399c3cd84b268
Unknown <tikanuadedi@gmail.com>
2018-04-05 04:01:06 +0700

    update

--- application/views/template/logo.php
+++ application/views/template/logo.php
@@ -1,6 +1,6 @@
 <a href="<?php echo base_url(); ?>" class="logo">
 	<!-- mini logo for sidebar mini 50x50 pixels -->
-	<span class="logo-mini"><b>BRG</b></span>
+	<span class="logo-mini"><b>PM</b></span>
 	<!-- logo for regular state and mobile devices -->
-	<span class="logo-lg"><b>BORANG</b></span>
+	<span class="logo-lg"><b>Prestasi Mahasiswa</b></span>
 </a>
